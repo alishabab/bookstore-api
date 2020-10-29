404dbfe26abb361d19add94132c91bc146dbbfe8
Shabab Ali <shababsaifi@gmail.com>
2020-10-29 18:11:19 +0530

    added jwt authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_094059) do
+ActiveRecord::Schema.define(version: 2020_10_29_104129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_10_29_094059) do
     t.string "author"
     t.string "category"
     t.integer "percentage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
